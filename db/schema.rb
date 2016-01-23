0643ea0c22cf75672831b5eb776f3bc4e26ab0d5
keskinbu <burakkeskin09@gmail.com>
2016-01-23 11:55:26 +0000

    Added Articles table and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160123114042) do
+ActiveRecord::Schema.define(version: 20160123114932) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.string   "tags"
+    t.string   "status"
+    t.integer  "author_id"
+    t.integer  "is_active"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
