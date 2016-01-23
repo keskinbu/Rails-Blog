19087d208afa293764f74c866f10e65cb06f2eef
keskinbu <burakkeskin09@gmail.com>
2016-01-23 16:17:11 +0000

    Added edit page and controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,6 +24,20 @@ class ArticlesController < ApplicationController
     end
   end
   
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:success] = "Your article was updated succesfully!"
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
+  end
+  
   private
   
     def article_params
