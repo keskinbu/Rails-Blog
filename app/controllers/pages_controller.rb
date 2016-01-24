2128f442c7fec727239dbc461d51fd78d17f917e
keskinbu <burakkeskin09@gmail.com>
2016-01-24 21:21:48 +0000

    Updated homepage css and type

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   
   def home
-    redirect_to articles_path if logged_in?
+    @articles = Article.paginate(page: params[:page], per_page: 3)
   end
   
   
