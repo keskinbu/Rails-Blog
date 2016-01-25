6ca0821fab2fe4243f731e04aaa2a748bba658c9
keskinbu <burakkeskin09@gmail.com>
2016-01-25 10:51:07 +0000

    Added Seo Friendly Article Url

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160125101411) do
+ActiveRecord::Schema.define(version: 20160125103303) do
 
   create_table "article_styles", force: :cascade do |t|
     t.integer "style_id"
@@ -29,7 +29,21 @@ ActiveRecord::Schema.define(version: 20160125101411) do
     t.datetime "updated_at"
     t.string   "picture"
     t.string   "summary"
+    t.string   "slug"
   end
+
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string   "slug",                      null: false
+    t.integer  "sluggable_id",              null: false
+    t.string   "sluggable_type", limit: 50
+    t.string   "scope"
+    t.datetime "created_at"
+  end
+
+  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
+  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
 
   create_table "likes", force: :cascade do |t|
     t.boolean  "like"
@@ -53,6 +67,7 @@ ActiveRecord::Schema.define(version: 20160125101411) do
     t.integer  "is_active"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "slug"
   end
 
 end
